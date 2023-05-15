c1af6626c581e4dc51291270e4e4b4818cf0efdd
gabriel8programmer <gabriel8github@gmail.com>
2023-05-15 15:23:34 -0300

    finish home of the site and starting the require burgers part

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="main-container">
-    <h1>Pedidos</h1>
+    <h1>Gerenciar Pedidos</h1>
+    <!--Dashboard with all request of the user-->
+    <DashBoard/>
   </div>
 </template>
+
+<script>
+
+import DashBoard from "../components/DashBoard.vue"
+
+export default {
+
+  name: "PedidosView",
+
+  components: {
+    DashBoard
+  }
+
+}
+
+
+</script>
+
